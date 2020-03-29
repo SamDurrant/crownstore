a7bb2abcc0a4f304b43ddc4374a9ae563cee7582
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-03-29 15:05:46 -0600

    update cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,8 +9,8 @@ const CollectionPreview = ({ title, items }) => (
       {/* filter collection for first four items and display a card for each */}
       {items
         .filter((item, i) => i < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
